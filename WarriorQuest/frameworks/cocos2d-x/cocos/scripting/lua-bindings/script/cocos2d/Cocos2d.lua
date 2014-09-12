fb2ba6872d5b8ca28803b1c525c76019bb2b4688
calfjohn <calfjohn@gmail.com>
2014-09-12 17:33:06 +0800

    1 Add 3d scene 2 add cc.V3 to cocos.lua

--- WarriorQuest/frameworks/cocos2d-x/cocos/scripting/lua-bindings/script/cocos2d/Cocos2d.lua
+++ WarriorQuest/frameworks/cocos2d-x/cocos/scripting/lua-bindings/script/cocos2d/Cocos2d.lua
@@ -362,3 +362,44 @@ end
 function cc.PhysicsMaterial(_density, _restitution, _friction)
 	return { density = _density, restitution = _restitution, friction = _friction }
 end
+
+--Vec3
+function cc.V3(p1, p2, p3)
+    return {x = p1, y = p2, z = p3}
+end
+
+function cc.V3Add(p1, p2)
+    return {x = p1.x + p2.x, y = p1.y + p2.y, z = p1.z + p2.z}
+end
+
+function cc.V3Sub(p1, p2)
+    return {x = p1.x - p2.x, y = p1.y - p2.y, z = p1.z - p2.z}
+end
+
+function cc.V3Mul(p1, p2)
+    return {x = p1.x * p2.x, y = p1.y * p2.y, z = p1.z * p2.z}
+end
+
+function cc.V3Dot(p1, p2)
+    return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z
+end
+
+function cc.V3MulEx(p1, p2)
+    return {x = p1.x * p2, y = p1.y * p2, z = p1.z * p2}
+end
+
+function cc.V3LengthSquared(p)
+    return (p.x * p.x + p.y * p.y + p.z * p.z)
+end
+
+function cc.V3Normalize(p)
+    local n = cc.V3LengthSquared(p)
+    if n == 1.0 then return end
+    
+    n = math.sqrt(n);
+    if n < 2e-37 then return end
+    
+    n = 1.0 / n;
+    p = cc.V3MulEx(p, n)
+end
+
