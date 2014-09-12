1f174f04ae55bbb0301c2b2f7f2741bc75eb009c
gaohuang <2933682586@qq.com>
2014-09-12 11:53:21 +0800

    camera and loading

--- WarriorQuest/src/main.lua
+++ WarriorQuest/src/main.lua
@@ -27,7 +27,8 @@ local function main()
     
     --create scene 
     --local scene = require("ChooseRoleScene")
-    local scene = require("BattleFieldScene")
+--    local scene = require("ActivateGameScene")
+    local scene = require("LoadingScene")
     --local scene = require("MainMenuScene")
     local activateGameScene = scene.create()
     --activateGameScene:playBgMusic()
