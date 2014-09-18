73824beb0b79378292d3a7da911575d5ddfbbc21
gaohuang <2933682586@qq.com>
2014-09-18 10:08:44 +0800

    mainmenuscene

--- WarriorQuest/src/ActivateGameScene.lua
+++ WarriorQuest/src/ActivateGameScene.lua
@@ -13,95 +13,63 @@ end
 
 function ActivateGame:create()
     local activate = ActivateGame:new()
-    local layer = activate:init()
+    --add layer
+    local layer = activate:createLayer()
     activate:addChild(layer)
+    
     return activate
 end
 
-function ActivateGame:init()
+function ActivateGame:createLayer()
     local layer = cc.Layer:create()
-
-    local player = cc.Sprite3D:create("Sprite3DTest/girl.c3b")
-    player:setPosition3D({x=self.size.width/2,y=self.size.height*0.45,z=0})
-    player:setRotation3D({x=0,y=90,z=0})
-    player:setCameraMask(2)
-    layer:addChild(player)
     
-    local animation = cc.Animation3D:create("Sprite3DTest/girl.c3b")
-    local animate = cc.Animate3D:create(animation)
-    player:runAction(cc.RepeatForever:create(animate))
+    -- add bg
+    local bg = cc.Sprite:create("activate_background.jpg")
+    bg:setPosition(self.size.width/2,self.size.height/2)
+    layer:addChild(bg,0)
     
-    --create camera
-    local camera = cc.Camera:createPerspective(40,self.size.width/self.size.height,1,1000)
-    camera:setPosition3D({x=self.size.width*0.2,y=self.size.height*0.9,z=0})
-    camera:lookAt(player:getPosition3D(),{x=0,y=1,z=0})
-    camera:setCameraFlag(2)
-    layer:addChild(camera)
+    -- add hero
+    self:addHero(layer)
     
-    --schedule
-    local offset = 1
-    local function schedule_callback()
-        local x = camera:getPositionX()
-        if x <= -400 then
-            offset = -1
-        elseif x>=self.size.width*0.15 then
-            offset = 1
-        end
-        camera:setPositionX(x-offset)
-        camera:lookAt(player:getPosition3D(),{x=0,y=1,z=0})
-    end
-    cc.Director:getInstance():getScheduler():scheduleScriptFunc(schedule_callback,0.005,false)
-
-    --create player2
-    local player2 = cc.Sprite3D:create("Sprite3DTest/girl.c3b")
-    player2:setPosition3D({x=self.size.width*0.2,y=self.size.height*0.45,z=0})
-    player2:setRotation3D({x=0,y=50,z=0})
-    player2:setCameraMask(4)
-    layer:addChild(player2)
-
-    local animation2 = cc.Animation3D:create("Sprite3DTest/girl.c3b")
-    local animate2 = cc.Animate3D:create(animation2)
-    player2:runAction(cc.RepeatForever:create(animate2))
-    
-    --create camera2
-    local camera2 = cc.Camera:createPerspective(50,self.size.width/self.size.height,1,1000)
-    camera2:setPosition3D({x=self.size.width*0.2,y=self.size.height*0.45,z=300})
-    camera2:lookAt(player2:getPosition3D(),{x=0,y=1,z=0})
-    camera2:setCameraFlag(4)
-    layer:addChild(camera2)
-    
-    player2:setPositionX(self.size.width*0.08)
-    player2:setPositionY(self.size.height*0.4)
-    
-    --schedule2
-    local point_y = 0.1
-    local point_z = 0.1
-    local schedule2_callback = function ()
-        if camera2:getPositionY() >= 350 then
-            point_y = point_y - 0.2
-        elseif camera2:getPositionY() <= self.size.height*0.45 then
-            point_y = point_y + 0.2
-        end
-        
-        if camera2:getPositionZ() <= 220 then
-            point_z = point_z - 0.2
-        elseif camera2:getPositionZ() >= 330 then
-            point_z = point_z + 0.2
-        end
-        camera2:setPositionY(point_y+camera2:getPositionY())
-        camera2:setPositionZ(camera2:getPositionZ()-point_z)
-        
-    end
-    cc.Director:getInstance():getScheduler():scheduleScriptFunc(schedule2_callback,0.01,false)
-    
+    --addCamera
+--    self:addCamera(layer)
+--    layer:setCameraMask(2)
 
     return layer
 end
+
+--add camera
+function ActivateGame:addCamera(layer)
+    --add camera
+    local camera = cc.Camera:createPerspective(60,self.size.width/self.size.height,1,1000)
+    camera:setPosition3D({x=0,y=10,z=10})
+    camera:lookAt(self._hero:getPosition3D(),{x=0,y=1,z=0})
+    camera:setCameraFlag(2)
+    layer:addChild(camera)
+    
+    
+end
+
+--add hero
+function ActivateGame:addHero(layer)
+    --add hero
+    local hero = require("Hero").create(0)
+    hero:setScale(8.0)
+    hero:setRotation3D({x=0,y=-60,z=0})
+    hero:setPosition({x=self.size.width*0.95, y=self.size.height*0.15})   
+    layer:addChild(hero)
+    self._hero = hero
+
+    --action   
+    local controlPoint = {{x=0,y=0},
+        {x=-self.size.width*0.25,y=-self.size.height*0.18},
+        {x=-self.size.width*0.5,y=-self.size.height*0.06},
+        {x=-self.size.width*0.58,y=self.size.height*0.07},
+        {x=-self.size.width*0.59,y=self.size.height*0.17},
+        {x=-self.size.width*0.73,y=self.size.height*0.28}}
+    local cardinalSpline = cc.CardinalSplineBy:create(3,controlPoint,0)
+    hero:runAction(cardinalSpline)
+end
         
   
-        
-        
-        
-        
-
 return ActivateGame
--- WarriorQuest/src/main.lua
+++ WarriorQuest/src/main.lua
@@ -23,15 +23,32 @@ local function main()
     
     cc.FileUtils:getInstance():addSearchPath("src")
     cc.FileUtils:getInstance():addSearchPath("res")
-    cc.Director:getInstance():getOpenGLView():setDesignResolutionSize(1136, 640, 0)
+    
+    --适配
+    local pEGLView = cc.Director:getInstance():getOpenGLView()
+    local frameSize = pEGLView:getFrameSize()
+    local winSize = {width=1136,height=640}
+
+    local widthRate = frameSize.width/winSize.width
+    local heightRate = frameSize.height/winSize.height
+
+    --    如果是if中的语句，说明逻辑的高度有点大了，就把逻辑的高缩小到和宽度一样的比率
+    if widthRate > heightRate then
+        --    里边传入的前俩个参数就是逻辑分辨率的大小，也就是通过getWinSize()得到的大小
+        pEGLView:setDesignResolutionSize(winSize.width,
+            winSize.height*heightRate/widthRate, 1)
+    else
+        pEGLView:setDesignResolutionSize(winSize.width*widthRate/heightRate, winSize.height,
+            1)
+    end
     
     --create scene 
     --local scene = require("ChooseRoleScene")
---    local scene = require("ActivateGameScene")
-    local scene = require("LoadingScene")
+    local scene = require("ActivateGameScene")
+--    local scene = require("LoadingScene")
 --    local scene = require("ChooseRoleScene")
     --local scene = require("BattleFieldScene")
-    --local scene = require("MainMenuScene")
+--    local scene = require("MainMenuScene")
     local activateGameScene = scene.create()
     --activateGameScene:playBgMusic()
     
